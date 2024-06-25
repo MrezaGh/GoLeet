340e2c30caa2a03bdaa9b40b43cef86ba27c7ec6
Mreza Gh <mrezagh.legend@gmail.com>
2024-06-25 19:30:26 +0330

    Solve linked-list-cycle more efficient

--- linked-list-cycle-easy/linked-list-cycle.go
+++ linked-list-cycle-easy/linked-list-cycle.go
@@ -5,18 +5,35 @@ type ListNode struct {
 	Next *ListNode
 }
 
+//func hasCycle(head *ListNode) bool {
+//	visited := make(map[*ListNode]bool)
+//	cur := head
+//	for cur != nil {
+//		if _, ok := visited[cur]; ok {
+//			return true
+//		} else {
+//			visited[cur] = true
+//		}
+//		cur = cur.Next
+//	}
+//	return false
+//}
+
 func hasCycle(head *ListNode) bool {
-	visited := make(map[*ListNode]bool)
-	cur := head
-	for cur != nil {
-		if _, ok := visited[cur]; ok {
-			return true
-		} else {
-			visited[cur] = true
-		}
-		cur = cur.Next
+	// constant space
+	if head == nil || head.Next == nil {
+		return false
 	}
-	return false
+	slow, fast := head, head.Next
+
+	for slow != fast {
+		if fast.Next == nil || fast.Next.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
 }
 
 func main() {
