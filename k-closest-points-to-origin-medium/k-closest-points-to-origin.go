c07a77479094ec139a73a2732e910889bd10c5ab
Mreza <mrezagh.legend@gmail.com>
2025-01-17 14:32:38 +0330

    ✔️ Solve k-closest-points-to-origin-medium

--- k-closest-points-to-origin-medium/k-closest-points-to-origin.go
+++ k-closest-points-to-origin-medium/k-closest-points-to-origin.go
@@ -7,24 +7,29 @@ import (
 )
 
 func kClosest(points [][]int, k int) [][]int {
-	distances := make([][2]int, 0) // [distances to origin, point index]
+	distances := make([][]int, 0) // [distances to origin, point index]
 
 	for i, point := range points {
 		distance := point[0]*point[0] + point[1]*point[1]
-		distances = append(distances, [2]int{distance, i})
+		distances = append(distances, []int{distance, i})
 	}
 
-	slices.SortedFunc(slices.Values(distances), func(e [2]int, e2 [2]int) int {
+	slices.SortFunc(distances, func(e []int, e2 []int) int {
 		return cmp.Compare(e[0], e2[0])
 	})
-	fmt.Println(distances)
 
-	return nil
+	//fmt.Println(distances)
+	ans := make([][]int, 0)
+	for i := 0; i < k; i++ {
+		ans = append(ans, points[distances[i][1]])
+	}
+
+	return ans
 }
 
 func main() {
-	points, k := [][]int{{1, 3}, {-2, 2}}, 1
-	//points, k := [][]int{{3,3},{5,-1},{-2,4}}, 2
+	//points, k := [][]int{{1, 3}, {-2, 2}}, 1
+	points, k := [][]int{{3, 3}, {5, -1}, {-2, 4}}, 2
 
 	fmt.Println(kClosest(points, k))
 }
