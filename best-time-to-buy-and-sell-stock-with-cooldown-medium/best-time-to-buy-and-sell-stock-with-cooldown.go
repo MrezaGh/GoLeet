724a43c77710e5e3511afba0f45b80a93b309d22
Mreza <mrezagh.legend@gmail.com>
2025-02-09 14:23:52 +0330

    ✅Solve  best-time-to-buy-and-sell-stock-with-cooldown real

--- best-time-to-buy-and-sell-stock-with-cooldown-medium/best-time-to-buy-and-sell-stock-with-cooldown.go
+++ best-time-to-buy-and-sell-stock-with-cooldown-medium/best-time-to-buy-and-sell-stock-with-cooldown.go
@@ -1,1 +1,46 @@
-package best_time_to_buy_and_sell_stock_with_cooldown_medium
+package main
+
+import (
+	"fmt"
+)
+
+type status struct {
+	buy   bool
+	index int
+}
+
+func maxProfit(prices []int) int {
+	n := len(prices)
+	seen := make(map[status]int)
+	var dfs func(buy bool, index int) int
+	dfs = func(buy bool, index int) int {
+		if index >= n {
+			return 0
+		} else if ans, exist := seen[status{buy, index}]; exist {
+			return ans
+		}
+		result := 0
+		if buy {
+			here := dfs(false, index+1) - prices[index]
+			next := dfs(true, index+1)
+			result = max(here, next)
+		} else {
+			here := dfs(true, index+2) + prices[index]
+			next := dfs(false, index+1)
+			result = max(here, next)
+		}
+		seen[status{buy, index}] = result
+		return result
+	}
+
+	maxP := dfs(true, 0)
+	return maxP
+}
+
+func main() {
+	//prices := []int{1, 2, 3, 0, 2}
+	//prices := []int{1}
+	prices := []int{2, 1, 4}
+
+	fmt.Println(maxProfit(prices))
+}
